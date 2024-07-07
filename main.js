dc6bdc051184d2168f3a58d4ad2ebc6d8e9cdce0
SadiaRazzaqJutt <sadiarazzaq14@gmail.com>
2024-07-07 17:19:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 20000; // rupees
+let myPin = 4567;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin number",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number"
+            }
+        ]);
+        // = += -=
+        myBalance -= amountAns.amount;
+        console.log("Your remaining balance is" + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log(`Your remaining balance is ${myBalance}`);
+    }
+    else {
+        console.log("Your pin is incorrect");
+    }
+}
